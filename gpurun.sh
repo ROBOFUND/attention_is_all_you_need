99e39d93c617aef3343cfd501dc4fbce81aeea4f
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-07-11 11:46:22 +0900

    長さを制限したデータセット

--- gpurun.sh
+++ gpurun.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-SOURCE=wakati/summarize-source.txt
-TARGET=wakati/summarize-target.txt
+SOURCE=wakati/filtered-200-summarize-source.txt
+TARGET=wakati/filtered-200-summarize-target.txt
 
 PYTHONIOENCODING=utf-8
 export PYTHONIOENCODING
 
-python -u train.py -g 0 -o result-sum -i '' \
+python -u train.py -g 0 -o result-sum-f200 -i '' \
        -s $SOURCE -t $TARGET --source-valid $SOURCE --target-valid $TARGET \
-       2>&1 | tee log-sum
+       2>&1 | tee log-sum-f200.txt
