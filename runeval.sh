ccbeb3cead30ad2ae7fb473cfc1903a13c789485
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-07-17 12:08:14 +0900

    ディレクトリプレフィックスをカラにする

--- runeval.sh
+++ runeval.sh
@@ -7,5 +7,5 @@ MODEL=result-sum-f200/best_model.npz
 PYTHONIOENCODING=utf-8
 export PYTHONIOENCODING
 
-python -u eval.py -g -1 \
+python -u eval.py -g -1 -i '' \
        -s $SOURCE -t $TARGET --model $MODEL
